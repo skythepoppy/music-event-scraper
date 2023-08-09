51b913f984bdfe3a82fe0e51fd6c488dd69405e2
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-09 11:16:27 -0500

    implemented selecting, adding and deleting sql data, and stored app data into sql database

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ import selectorlib
 import smtplib, ssl
 import os
 import time
+import sqlite3
 
 
+
+# establish a connection
+connection = sqlite3.connect("data.db")
+
+# url and headers
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 '
                   '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+
+# scrape page source from the URL
 def scrape(url):
     """ Scrape the page source from the URL """
     response = requests.get(url, headers=HEADERS)    #
     source = response.text      # extract text from url
     return source
 
+# extract data
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
+# send information to email
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
@@ -35,13 +45,25 @@ def send_email(message):
 
     print("Email was sent!")
 
-def store(extracted):
-    with open("data.txt", "a") as file:     # append data
-        return file.write(extracted + "\n")
 
+# store extracted data into database
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()    # initialize cursor object
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+
+# read in data extracted from the scraped URL
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()    # initialize cursor object
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))  # cursor object can execute sql queries
+    rows = cursor.fetchall()
+    return rows
+
 
 if __name__ == "__main__":
     # runs program non-stop ( in 2 second intervals)
@@ -50,9 +72,10 @@ if __name__ == "__main__":
         extracted = extract(scraped)
         print(extracted)
 
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="New event was found!")
 
